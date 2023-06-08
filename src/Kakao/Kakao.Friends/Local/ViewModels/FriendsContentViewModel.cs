0c584528b044d9a16966526d1b9eefe08d4e9233
Doit <duhee7524@naver.com>
2023-06-08 10:38:24 +0900

    .

--- src/Kakao/Kakao.Friends/Local/ViewModels/FriendsContentViewModel.cs
+++ src/Kakao/Kakao.Friends/Local/ViewModels/FriendsContentViewModel.cs
@@ -1,5 +1,8 @@
 ﻿using CommunityToolkit.Mvvm.Input;
+using Jamesnet.Wpf.Controls;
 using Jamesnet.Wpf.Mvvm;
+using Prism.Ioc;
+using Prism.Regions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,15 +13,26 @@ namespace Kakao.Friends.Local.ViewModels
 {
     public partial class FriendsContentViewModel : ObservableBase
     {
-        public FriendsContentViewModel()
-        {
+        private readonly IRegionManager _regionManager;
+        private readonly IContainerProvider _containerProvider;
 
+        public FriendsContentViewModel(IRegionManager regionManager, IContainerProvider containerProvider)
+        {
+            _regionManager = regionManager;
+            _containerProvider = containerProvider;
         }
 
         [RelayCommand]
-        private void Login()
+        private void Logout()
         {
+            IRegion mainRegion = _regionManager.Regions["MainRegion"];
+            IViewable loginContent = _containerProvider.Resolve<IViewable>("LoginContent");
 
+            if (mainRegion.Views.Contains(loginContent))
+            {
+                mainRegion.Add(loginContent);
+            }
+            mainRegion.Activate(loginContent);
         }
     }
 }
